62f9581908af6f8bc8365acea961d25abe1610b3
Andrey <rolland81280@gmail.com>
2022-09-12 01:34:34 +0300

    сохран9

--- Problem-23/Program.cs
+++ Problem-23/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает 
+// на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число ");
+string name = Console.ReadLine();
+int number = Convert.ToInt32(name);
+int[] array = new int[number];
+for (int i = 1; i < number + 1; i++)
+{
+    System.Console.Write(i * i * i + " ");
+}
